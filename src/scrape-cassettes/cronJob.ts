699133450f8fe0e3d6985334d5eef7875f60b0e7
Qodesmith <theqodesmith@gmail.com>
2024-06-16 19:55:48 -0400

    Added scrap-cassettes project

---
+++ src/scrape-cassettes/cronJob.ts
@@ -0,0 +1,54 @@
+import {CronJob} from 'cron'
+import {timeZone} from '../../common/timeZone'
+import {scrapeCassettes} from './scrapeCassettes'
+import {log} from '@qodestack/utils'
+
+/*
+
+  field          allowed values
+  -----          --------------
+  second         0-59
+  minute         0-59
+  hour           0-23
+  day of month   1-31
+  month          1-12 (or names, see below)
+  day of week    0-7 (0 or 7 is Sunday, or use names)
+
+  The `cron` package uses a 6-slot cron syntax, the first slot being seconds.
+  You can also use a regular Unix 5-slot syntax which will default the seconds
+  slot to 0.
+
+*/
+
+const job = CronJob.from({
+  cronTime: Bun.env.CRON_TIME ?? '0 44 1 * * 1', // Every Monday at 1:44am
+  start: true,
+  timeZone,
+  onTick: handleJob,
+})
+
+async function handleJob() {
+  try {
+    log.text('Downloading cassettes...')
+    const {duplicates, errors, success, itemsForDownload, itemsOnPage} =
+      await scrapeCassettes()
+
+    if (errors.length) {
+      log.error(errors.length, 'failed downloads.')
+    }
+
+    if (success.length) {
+      log.success(success.length, 'images downloaded!')
+    }
+
+    if (!errors.length && !success.length) {
+      log.text('No images to download!')
+    }
+  } catch (error) {
+    log.error('`scrapeCassettes` process failed:', error)
+  }
+
+  console.log('-'.repeat(100))
+}
+
+handleJob()
---
+++ src/scrape-cassettes/scrapeCassettes.ts
@@ -0,0 +1,72 @@
+import {chunkArray, invariant} from '@qodestack/utils'
+import {load} from 'cheerio'
+import path from 'node:path'
+
+type FileData = {url: string; fileName: string}
+
+const destination = Bun.env.DESTINATION
+invariant(destination, '`DESTINATION` env var not set')
+
+export async function scrapeCassettes() {
+  const res = await fetch('http://www.tapedeck.org/archive.php')
+  const html = await res.text()
+  const $ = load(html)
+  const data: FileData[] = []
+
+  const imgNodes = $('.tape img')
+
+  imgNodes.each((i, el) => {
+    const url = $(el).attr('src')
+
+    if (url) {
+      const {base} = path.parse(url)
+      const cassetteUrl = url.replace('_thumb', '').replace('-thumb', '')
+      const fileName = base.replace('_thumb', '').replace('-thumb', '')
+
+      data.push({url: cassetteUrl, fileName})
+    }
+  })
+
+  const chunkedData = chunkArray(data, 5)
+  const errors: FileData[] = []
+  const success: FileData[] = []
+  const duplicates: FileData[] = []
+
+  for (const items of chunkedData) {
+    const promises = items.reduce<Promise<void>[]>((acc, item) => {
+      const destinationPath = `${destination}/${item.fileName}`
+
+      // Only download files we don't already have.
+      if (!Bun.file(destinationPath).size) {
+        acc.push(downloadCassetteImage(item))
+      } else {
+        duplicates.push(item)
+      }
+
+      return acc
+    }, [])
+
+    const batchResults = await Promise.allSettled(promises)
+
+    batchResults.forEach((res, i) => {
+      const item = items[i]
+      res.status === 'fulfilled' ? success.push(item) : errors.push(item)
+    })
+  }
+
+  return {
+    errors,
+    success,
+    itemsOnPage: imgNodes.length,
+    itemsForDownload: data.length,
+    duplicates,
+  }
+}
+
+async function downloadCassetteImage({url, fileName}: FileData): Promise<void> {
+  const res = await fetch(url)
+  const arrayBuffer = await res.arrayBuffer()
+  const destinationPath = `${destination}/${fileName}`
+
+  return void Bun.write(destinationPath, arrayBuffer)
+}
