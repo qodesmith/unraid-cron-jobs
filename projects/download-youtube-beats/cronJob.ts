a91f9fd5e9e585b0f958d3e415d1cf5e95f9e249
Qodesmith <theqodesmith@gmail.com>
2024-06-22 10:46:07 -0400

    Added download-youtube-beats project

---
+++ projects/download-youtube-beats/cronJob.ts
@@ -0,0 +1,87 @@
+import {CronJob} from 'cron'
+import {downloadYoutubeBeats} from './downloadYoutubeBeats'
+import {timeZone} from '../../common/timeZone'
+import {logJobEndMessage} from '../../common/logJobEndMessage'
+import dotenv from 'dotenv'
+import {createLogger} from '@qodestack/utils'
+import {$} from 'bun'
+
+/*
+
+  field          allowed values
+  -----          --------------
+  second         0-59
+  minute         0-59
+  hour           0-23
+  day of month   1-31
+  month          1-12 (or names, see below)
+  day of week    0-7 (0 or 7 is Sunday, or use names)
+
+  The `cron` package uses a 6-slot cron syntax, the first slot being seconds.
+  You can also use a regular Unix 5-slot syntax which will default the seconds
+  slot to 0.
+
+*/
+
+// Load secret env vars from the Unraid server.
+dotenv.config({path: '/env/download-youtube-beats.env'})
+
+const fullJob = CronJob.from({
+  cronTime: Bun.env.CRON_TIME_FULL_JOB ?? '0 15 2 * * *', // Every day at 2:15am
+  start: true,
+  timeZone,
+  onTick: () => handleJob({isFullJob: true, updateDeps: true}),
+})
+
+const job = CronJob.from({
+  cronTime: Bun.env.CRON_TIME ?? '0 15 0,4-23 * * *', // Every 2 (even) hours, 15 past the hour, EXCEPT 2:15am.
+  start: true,
+  timeZone,
+  onTick: () => handleJob({isFullJob: false}),
+})
+
+export async function handleJob({
+  isFullJob,
+  updateDeps,
+}: {
+  isFullJob: boolean
+  updateDeps?: boolean
+}) {
+  const log = createLogger({timeZone})
+
+  if (updateDeps) {
+    await updateDependencies()
+  }
+
+  const initialMsg = isFullJob
+    ? 'Running full beats download'
+    : 'Downloading beats'
+
+  log.text(`${initialMsg}...`)
+
+  await downloadYoutubeBeats({isFullJob})
+
+  logJobEndMessage(isFullJob ? fullJob : job)
+}
+
+async function updateDependencies() {
+  const log = createLogger({timeZone})
+
+  log.text('Updating yt-dlp...')
+
+  const ytdlpRes = await $`yt-dlp --update`.quiet().nothrow()
+  if (ytdlpRes.exitCode !== 0) {
+    log.error('Failed to update yt-dlp:', ytdlpRes.stderr.toString())
+  } else {
+    log.text('yt-dlp update complete')
+  }
+
+  log.text('Updating ffmpeg...')
+
+  const ffmpegRes = await $`apt-get install ffmpeg`.quiet().nothrow()
+  if (ffmpegRes.exitCode !== 0) {
+    log.error('Failed to update ffmpeg:', ffmpegRes.stderr.toString())
+  } else {
+    log.text('ffmpeg update complete')
+  }
+}
---
+++ projects/download-youtube-beats/downloadYoutubeBeats.ts
@@ -0,0 +1,118 @@
+import {
+  downloadYouTubePlaylist,
+  type DownloadType,
+  type Results,
+} from '@qodestack/dl-yt-playlist'
+import {getLocalDate, invariant} from '@qodestack/utils'
+import {createLogger} from '@qodestack/utils'
+import {timeZone} from '../../common/timeZone'
+
+type FinalResultsObj = Results & {
+  date: string
+  job: 'beats' | 'fullBeats'
+  hasErrors: boolean
+}
+
+export async function downloadYoutubeBeats({isFullJob}: {isFullJob: boolean}) {
+  const log = createLogger({timeZone})
+
+  try {
+    const {
+      playlistId,
+      youTubeApiKey,
+      directory,
+      downloadType,
+      audioFormat,
+      videoFormat,
+      downloadThumbnails,
+      maxDurationSeconds,
+      mostRecentItemsCount,
+      silent,
+      maxConcurrentFetchCalls,
+      maxConcurrentYtdlpCalls,
+      saveRawResponses,
+    } = (() => {
+      const {
+        PLAYLIST_ID,
+        YOUTUBE_API_KEY,
+        DESTINATION,
+        DOWNLOAD_TYPE,
+        AUDIO_FORMAT,
+        VIDEO_FORMAT,
+      } = Bun.env
+      invariant(PLAYLIST_ID, 'PLAYLIST_ID env var not found')
+      invariant(YOUTUBE_API_KEY, 'YOUTUBE_API_KEY env var not found')
+      invariant(DESTINATION, 'DESTINATION env var not found')
+      invariant(DOWNLOAD_TYPE, 'DOWNLOAD_TYPE env var not found')
+
+      return {
+        playlistId: PLAYLIST_ID,
+        youTubeApiKey: YOUTUBE_API_KEY,
+        directory: DESTINATION,
+        downloadType: DOWNLOAD_TYPE as DownloadType,
+        audioFormat: AUDIO_FORMAT,
+        videoFormat: VIDEO_FORMAT,
+        downloadThumbnails: Bun.env.DOWNLOAD_THUMBNAILS === 'true',
+        maxDurationSeconds: Bun.env.MAX_DURATION_SECONDS
+          ? Number(Bun.env.MAX_DURATION_SECONDS)
+          : undefined,
+        mostRecentItemsCount: Bun.env.MOST_RECENT_ITEMS_COUNT
+          ? Number(Bun.env.MOST_RECENT_ITEMS_COUNT)
+          : undefined,
+        silent: Bun.env.SILENT === 'true',
+        maxConcurrentFetchCalls: Bun.env.MAX_CONCURRENT_FETCH_CALLS
+          ? Number(Bun.env.MAX_CONCURRENT_FETCH_CALLS)
+          : undefined,
+        maxConcurrentYtdlpCalls: Bun.env.MAX_CONCURRENT_YTDLP_CALLS
+          ? Number(Bun.env.MAX_CONCURRENT_YTDLP_CALLS)
+          : undefined,
+        saveRawResponses: Bun.env.SAVE_RAW_RESPONSES === 'true',
+      }
+    })()
+
+    const results = await downloadYouTubePlaylist({
+      playlistId,
+      youTubeApiKey,
+      directory,
+      downloadType,
+      audioFormat,
+      videoFormat,
+      downloadThumbnails,
+      maxDurationSeconds,
+      mostRecentItemsCount: isFullJob ? undefined : mostRecentItemsCount,
+      silent,
+      timeZone,
+      maxConcurrentFetchCalls,
+      maxConcurrentYtdlpCalls,
+      saveRawResponses,
+    })
+
+    try {
+      const resultsFilePath = `${directory}/results.json`
+      const resultsList: FinalResultsObj[] = await (async () => {
+        try {
+          return await Bun.file(resultsFilePath).json()
+        } catch {
+          return []
+        }
+      })()
+
+      resultsList.unshift({
+        date: getLocalDate(),
+        job: isFullJob ? 'fullBeats' : 'beats',
+        hasErrors: resultsHaveErrors(results),
+        ...results,
+      })
+
+      await Bun.write(resultsFilePath, JSON.stringify(resultsList, null, 2))
+    } catch {
+      log.error('Failed to write results')
+    }
+  } catch (error: any) {
+    log.error('Job failed:', error?.message)
+  }
+}
+
+function resultsHaveErrors(results: Results): boolean {
+  return Object.values(results.failureData).some(errArr => !!errArr.length)
+}
